283bf490568c8dbe9f18ad78c5bb26346ca1f9e0
Fortune Zviregei <fortunechainz@gmail.com>
2024-02-18 19:54:58 +0100

    Delete unused test files and update dependencies

--- app/utils/add.test.ts
+++
@@ -1,5 +0,0 @@
-import { add } from './add'
-
-test('add function correctly adds two numbers', () => {
-  expect(add(1, 2)).toBe(3)
-})
--- e2e/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('has text Hello World', async ({ page }) => {
-  await page.goto(`/`)
-
-  const title = await page.innerText('h1')
-  expect(title).toBe('Hello World')
-})
