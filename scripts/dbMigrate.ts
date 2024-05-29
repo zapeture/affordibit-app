82eee0fcd3f427ac7d564bebdea73f29801e572b
Fortune Zviregei <fortunechainz@gmail.com>
2024-05-29 12:38:36 +0200

    refactor(src): made core chnages
    configure db and file formatting and linting

---
+++ scripts/dbMigrate.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+
+import { Env } from '@/libs/Env';
+
+const client = postgres(Env.DATABASE_URL, { max: 1 });
+
+async function main() {
+  console.log('Migration started');
+
+  await migrate(drizzle(client), { migrationsFolder: './migrations' });
+
+  console.log('Migration completed');
+
+  await client.end();
+
+  process.exit(0);
+}
+
+main().catch((error) => {
+  console.error('Migration failed');
+  console.log(error);
+  process.exit(1);
+});
---
+++ src/libs/DB.ts
@@ -0,0 +1,16 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+
+import { Env } from './Env';
+
+const client = postgres(Env.DATABASE_URL, { max: 1 });
+
+export const db = drizzle(client);
+
+// Disable migrate function if using Edge runtime and use `npm run db:migrate` instead.
+// Only run migrate in development. Otherwise, migrate will also be run during the build which can cause errors.
+// Migrate during the build can cause errors due to the locked database when multiple migrations are running at the same time.
+if (process.env.NODE_ENV === 'development') {
+  await migrate(db, { migrationsFolder: './migrations' });
+}
