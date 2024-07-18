5fb12b61c1a59fecfe6c980ff86db8ac873814f2
Fortune Zviregei <fortunechainz@gmail.com>
2024-07-18 18:10:29 +0200

    chore: remove dashboard components and update dependencies

--- src/app/[locale]/(auth)/dashboard/layout.tsx
+++ src/app/[locale]/(auth)/dashboard/layout.tsx
@@ -1,10 +1,3 @@
-import DashboardSidebar from "@/templates/dashboard/DashboardSidebar";
-import DashboardTemplate from "@/templates/dashboard/DashboardTemplate";
-
-export default function DashboardLayout(props: { children: React.ReactNode }) {
-  return (
-    <DashboardTemplate sidebar={<DashboardSidebar />}>
-      {props.children}
-    </DashboardTemplate>
-  );
+export default function DashboardLayout() {
+  return <div>Hello</div>;
 }
--- src/templates/dashboard/Header.tsx
+++
@@ -1,9 +0,0 @@
-import AffordibitLogo from "@/components/branding/AffordibitLogo";
-
-export default function Header() {
-  return (
-    <header className="flex min-h-[88px] flex-none items-center justify-center">
-      <AffordibitLogo variant="light" />
-    </header>
-  );
-}
