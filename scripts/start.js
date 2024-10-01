f8b288adf3e465f6ffac0c764436030d134665ab
Fortune Zviregei <fortunechainz@gmail.com>
2024-10-01 19:50:50 +0200

    Remove unnecessary scripts and dependencies

--- scripts/start.js
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-import { execa } from 'execa';
-
-const { stdout } = await execa({ stdout: ['inherit'] })`next start --port ${process.env.APP_PORT || 3000}`;
-console.log(stdout);
--- scripts/turbo_dev.js
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-import { execa } from 'execa';
-
-const { stdout } = await execa({ stdout: ['inherit'] })`next dev --port ${process.env.APP_PORT || 3000} --turbo`;
-console.log(stdout);
